460e7901e283eb282fb71da8aa6466ff2df4bbe8
ваше имя < >
2019-01-28 12:48:05 +0600

    Added Sidebar

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './Header';
 import Footer from './Footer';
+import Sidebar from './Sidebar';
+import Content from './Content';
+import ContentItems from './ContentItems';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Header happy={'Счастливого пути!'}/>, document.getElementById('header'));
 ReactDOM.render(<Footer text={'Booking (c) Все права защищены'}/>, document.getElementById('footer'));
+
+const company = (<div>
+    <Sidebar name="Кыргызстан Аба Жолдору" />
+    <Sidebar name="Turkish Airlines" />
+    <Sidebar name="Aeroflot" />
+    <Sidebar name="Air Astana" />
+</div>);
+ReactDOM.render(company, document.getElementById('sidebar'));
 
 
 
